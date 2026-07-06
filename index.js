c4a3759f7fa3b0a3f1558ce594e2c19cbc5d7798
umarwaheed495-hub <umarwaheed495@gmail.com>
2026-07-06 18:41:10 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express');
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+   res.send('Hello World!')
+})
+
+app.get('/twitter',(req, res) => {
+    res.send('Umar Waheed');
+})
+
+app.get('/login', (req, res)=>{
+    res.send('<h1>please login at chai our code</h1>')
+})
+
+app.get('/Youtube', (req,res)=>{
+    res.send('<h2> chai aur code</h2>');
+})
+app.listen(process.env.PORT, () => {
+   console.log(`Example app listening on port ${port}`)
+})
